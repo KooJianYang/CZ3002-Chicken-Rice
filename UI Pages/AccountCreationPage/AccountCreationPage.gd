6292ec4e866560c8301c0128f7a782e2e55f6643
Daryl Neo <darylneojiaxun@hotmail.com>
2021-10-02 15:18:50 +0800

    Updated multiple pages, added message to some buttons.

--- UI Pages/AccountCreationPage/AccountCreationPage.gd
+++ UI Pages/AccountCreationPage/AccountCreationPage.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var curemail
 # export(PackedScene) var home_page
 
 func _ready():
@@ -8,12 +9,17 @@ func _ready():
 func _on_CreateAccountButton_pressed():
 	var email = $EmailInput.text
 	var password = $PasswordInput.text
+	if email.empty():
+		$Message.text= "Please enter Email"
+	elif password.empty():
+		$Message.text= "Please enter Password"
 	Firebase.Auth.signup_with_email_and_password(email, password)
 
 func _on_FirebaseAuth_signup_succeeded(auth_info):
-	print("Success" + str(auth_info))
+	curemail= auth_info.email
+	print("Success"+ curemail)
 	Firebase.Auth.send_account_verification_email()
-	# get_tree().change_scene_to(home_page)
+	get_tree().change_scene("res://UI Pages/InputAgePage/InputAgePage.tscn")
 
 func _on_Back_button_pressed():
 	get_tree().change_scene("res://UI Pages/LoginPage/LoginPage.tscn")
