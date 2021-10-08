36145047220ce31dfa5d5c671324a7de01235b1d
KooJianYang <jianyangkoo@gmail.com>
2021-10-08 17:01:44 +0800

    Memory Game update intruction page
    Added instruction page with step by step picture instructions.

--- UI Pages/InstructionPages/MemoryGame/MemoryGameInstructionPage.gd
+++ UI Pages/InstructionPages/MemoryGame/MemoryGameInstructionPage.gd
@@ -1,8 +1,41 @@
 extends Control
 
-func _ready():
-	pass
+onready var InstructionPic = $Instructions/Container/InstructionImages
+onready var LeftButton = $Pages/Left
+onready var RightButton = $Pages/Right
+onready var CurrentPage = $Pages/CurrentPage
+var stepone = load("res://Assets/MemoryGame/step 1.png")
+var steptwo = load("res://Assets/MemoryGame/step 2.png")
+var stepthree = load("res://Assets/MemoryGame/step 3.png")
+var stepfour = load("res://Assets/MemoryGame/step 4.png")
+var stepfive = load("res://Assets/MemoryGame/step 5.png")
+var stepsix = load("res://Assets/MemoryGame/step 6.png")
+var stepList = [stepone, steptwo, stepthree, stepfour, stepfive, stepsix]
+var current = 0
 
+func _ready():
+	InstructionPic.set_texture(stepone)
+	LeftButton.set_disabled(true)
+	CurrentPage.text = str(current+1)+"/6"
+
+
+func _on_Left_pressed():
+	current -= 1
+	InstructionPic.set_texture(stepList[current])
+	CurrentPage.text = str(current+1)+"/6"
+	if current == 0:
+		LeftButton.set_disabled(true)
+	if current < 5:
+		RightButton.set_disabled(false)
+
+func _on_Right_pressed():
+	current += 1
+	InstructionPic.set_texture(stepList[current])
+	CurrentPage.text = str(current+1)+"/6"
+	if current == 5:
+		RightButton.set_disabled(true)
+	if current > 0:
+		LeftButton.set_disabled(false)
 
 func _on_Easy_pressed():
 	GlobalScript.MemoryGameDifficulty = 0
@@ -22,4 +55,5 @@ func _on_PlayButton_pressed():
 
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://UI Pages/HomePage/HomePage.tscn")
+
 
