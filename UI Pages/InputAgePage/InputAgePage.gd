029093432bb345338bdddeed1b346799f64e622a
Shun Bin <shunbin.yeoh@hubble.sg>
2021-10-12 17:47:14 +0800

    stats/memory-game-stats-DB-working/fix-4-get-name-age-email

--- UI Pages/InputAgePage/InputAgePage.gd
+++ UI Pages/InputAgePage/InputAgePage.gd
@@ -9,8 +9,8 @@ func _on_ConfirmAgeButton_pressed():
 	#print("xd")
 	var Age = $VBoxContainer/AgeInput.text
 	$Message.text= "Successfully saved!"
-	firestore_collection.add(player_email,{'Name':player_email,'Age': Age, 'MScore': '', 'RScore': '', 'JScore': '', 'OScore': ''})
-	#{'Name': player_name, 'Email':player_email, 'Age': Age, 'MScore': '', 'RScore': '', 'JScore': '', 'OScore': ''}
+	firestore_collection.add(player_email,{'Email':player_email,'Age': Age, 'MScore': '', 'RScore': '', 'JScore': '', 'OScore': ''})
+	# need to add 'Name': player_name
 	get_tree().change_scene("res://UI Pages/LoginPage/LoginPage.tscn")
 
 #player_email.userinfo.email,{'Name':player_email.userinfo.email,'Age': 30}
