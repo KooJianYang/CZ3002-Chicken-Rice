8b9992f632412c685337b6569461c1293c026a83
KooJianYang <jianyangkoo@gmail.com>
2021-10-19 02:11:10 +0800

    buttons and aesthetic changes by daryl

--- UI Pages/AccountCreationPage/AccountCreationPage.gd
+++ UI Pages/AccountCreationPage/AccountCreationPage.gd
@@ -19,8 +19,9 @@ func _on_CreateAccountButton_pressed():
 		$Message.text= "Please enter Password"
 	elif len(password) < 6:
 		$Message.visible=true
-		$Message.text= "Password must have minimum of 6 characters!"
-	Firebase.Auth.signup_with_email_and_password(email, password)	
+		$Message.text= "Password must have minimum of 6 characters"
+	else:
+		Firebase.Auth.signup_with_email_and_password(email, password)	
 
 func _on_FirebaseAuth_signup_succeeded(auth_info):
 	curEmail= auth_info.email
@@ -32,9 +33,9 @@ func _on_FirebaseAuth_signup_failed(error_code, messages):
 	#$Message.visible=true
 	get_node("Message").visible=true
 	if (messages=="EMAIL_EXISTS"):
-		$Message.text= "Email already registered."
+		$Message.text= "Email already registered"
 	elif (messages=="INVALID_EMAIL"):
-		$Message.text= "Please enter a valid Email."
+		$Message.text= "Please enter a valid Email"
 	print("error code: " + str(error_code))
 	print("message: " + str(messages))
 
--- UI Pages/ResetPasswordPage/ResetPassword.gd
+++ UI Pages/ResetPasswordPage/ResetPassword.gd
@@ -10,7 +10,7 @@ func _on_ResetButton_pressed():
 	else:
 		Firebase.Auth.send_password_reset_email(email)
 		popup.visible=true
-		message.text = "Email has been sent! Please check your email to reset password."
+		message.text = "Email has been sent\nPlease check your email to reset password"
 
 func _on_Backbutton_pressed():
 	get_tree().change_scene("res://UI Pages/LoginPage2/LoginPage2.tscn")
